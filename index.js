9cf47c94df54a70294014303bad4a639f586a250
Puja Mandal <pujja6892@gmail.com>
2024-11-03 07:53:15 -0800

    added new post method

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3001
-
+// const port = 3001
+require('dotenv').config();
 app.get('/', (req, res) => {
   res.status(200).send('<h1>Hello World</h1>')
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/', (req, res) => {
+  res.status(200).send("this is my second commit")
+  console.log(req)
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
